fb3304ae67be6452855fde407b543b253f86cf43
yangwenliang <yang6233562@gmail.com>
2017-02-24 19:18:18 +0800

    重写navigator

--- index.ios.js
+++ index.ios.js
@@ -5,9 +5,10 @@ import {
   StyleSheet,
   Text,
   View,
-  NavigatorIOS,
+  Navigator,
   TouchableHighlight,
-  AsyncStorage
+  AsyncStorage,
+  TouchableOpacity
 } from 'react-native';
 
 // await 例子
@@ -35,48 +36,52 @@ const storageId = 'myList';
 
 let tools = {
   format: function (str){
-    // console.warn(typeof str);
     let s = (str < 10) ? ('0'+str) : str;
     return s;
   }
 }
+
+let globT = {};
 
 class Timer extends Component{
  
   constructor(props) {
     super(props);
   
-    this.state = {h: 0,m: 0,s: 0};
+    this.state = {hour: 0,minute: 0,second: 0};
   }
 
   addTime(o){
-    o.s++;
-    if(o.s >= 60){
-      o.s = o.s - 60;
-      o.m++
+    o.second++;
+    if(o.second >= 60){
+      o.second = o.second - 60;
+      o.minute++
     }
-    if(o.m >= 60){
-      o.m = o.m - 60;
-      o.h++
+    if(o.minute >= 60){
+      o.minute = o.minute - 60;
+      o.hour++
     }
 
     return o;
   }
   componentDidMount(){
     let time = this.props.time;
-    this.setState({h: time.hour,m: time.minute,s: time.second});
+    let h = time.hour || 0;
+    let m = time.minute || 0;
+    let s = time.second || 0;
+    this.setState({hour: h,minute: m,second: s});
 
     this.setInterval(() => {
       let timeO = this.state;
       let time = this.addTime(timeO);
       this.setState(time);
-      this.props.change(time);
+      globT = time;
     },1000);
   }
   render(){
     return (
       <View style={styles.timerWrap}>
-        <Text style={styles.timerTxt}>{tools.format(this.state.h)}:{tools.format(this.state.m)}:{tools.format(this.state.s)}</Text>
+        <Text style={styles.timerTxt}>{tools.format(this.state.hour)}:{tools.format(this.state.minute)}:{tools.format(this.state.second)}</Text>
       </View>
     );
   }
@@ -87,7 +92,7 @@ class Detail extends Component{
   render(){
     return (
       <View style={styles.detailWrap}>
-        <Timer change={this.props.change} time={this.props.time}/>
+        <Timer  time={this.props.time} />
       </View>
     );
   }
@@ -99,7 +104,6 @@ class Row extends Component{
   
     this.state = {id: 1,task: null};
 
-    this.change = this.change.bind(this);
   }
 
    _handleBackPress() {
@@ -119,48 +123,52 @@ class Row extends Component{
     
   }
 
-  change(v){
-    let task = this.state.task;
-    task.time.hour = v.h;
-    task.time.minute = v.m;
-    task.time.second = v.s;
-    this.setState(task);
-  }
-
   render(){
     const nextRoute = {
-      component: Detail,
+      // component: Detail,
+      // title: '修炼中',
+      // titleTextColor: '#fff',
+      // barTintColor: '#3b5999',
+      // tintColor: '#fff',
+      // passProps: { myProp: 'bar' ,change:this.change, time: this.props.task.time},
+      // leftButtonSystemIcon: 'done',
+      // onLeftButtonPress: ()=>{
+      //   // 利用await方式，可以保证state更新完毕再跳转。
+      //   let back = async ()=>{
+      //     await this.props.upDateTime(this.props.id , this.state.task).catch(
+      //       (err) => {console.warn(err)}
+      //     );
+      //     this._handleBackPress();
+      //   };
+      //   back();
+      // }
+      detail:{
+        index: this.props.id
+      },
       title: '修炼中',
-      titleTextColor: '#fff',
-      barTintColor: '#3b5999',
-      tintColor: '#fff',
-      passProps: { myProp: 'bar' ,change:this.change, time: this.props.task.time},
-      leftButtonSystemIcon: 'done',
-      onLeftButtonPress: ()=>{
-        // 利用await方式，可以保证state更新完毕再跳转。
-        let back = async ()=>{
-          await this.props.upDateTime(this.props.id , this.state.task).catch(
-            (err) => {console.warn(err)}
-          );
-          this._handleBackPress();
-        };
-        back();
-      }
+      index: this.props.id +1
     };
     let task = this.props.task;
-    return(
-      <View style={styles.list}>
-        <TouchableHighlight  
-        onPress={() => this._handleNextPress(nextRoute)}>
-        <View style={styles.row}>
-          <Text style={styles.listItemTitle}>{task.name}</Text>
-          <Text style={styles.listItem}>
-            {tools.format(task.time.hour)}:{tools.format(task.time.minute)}:{tools.format(task.time.second)}
-          </Text>
+    if(task.time){
+      return(
+        <View style={styles.list}>
+          <TouchableHighlight  
+          onPress={() => this._handleNextPress(nextRoute)}>
+          <View style={styles.row}>
+            <Text style={styles.listItemTitle}>{task.name}</Text>
+            <Text style={styles.listItem}>
+              {tools.format(task.time.hour)}:{tools.format(task.time.minute)}:{tools.format(task.time.second)}
+            </Text>
+          </View>
+          </TouchableHighlight>
         </View>
-        </TouchableHighlight>
-      </View>
-    );
+      );
+    }else{
+      return(
+        <View/>
+      );
+    }
+    
   }
 }
 
@@ -194,6 +202,50 @@ let taskList = {tasks:[
     ]};
 
 class Home extends Component{
+ 
+  componentWillUnmount(){
+    // ???什么时机触发？
+    // let tasks = this.state.tasks;
+  }
+  
+  render() {
+    let tasks = this.props.tasks || taskList;
+    return(
+      <View style={styles.home}>
+        {
+          tasks.map((v ,k) => {
+            return (
+            // <View/>
+            <Row navigator={this.props.navigator} 
+            id = {k}
+            key= {k} 
+            upDateTime = {this.props.upDateTime}
+            task = {v}
+            />
+            );
+          })
+        }
+      </View>
+    );
+  }
+}
+
+// export default class Life extends Component {
+//   render() {
+//     return (
+//       <NavigatorIOS
+//         initialRoute={{
+//           component: Home,
+//           title: 'Life',
+//           titleTextColor: '#fff',
+//           barTintColor: '#3b5999'
+//         }}
+//         style={styles.nav}
+//       />
+//     );
+//   }
+// }
+export default class Life extends Component {
   constructor(props) {
     super(props);
   
@@ -207,17 +259,17 @@ class Home extends Component{
         this.setState({tasks:JSON.parse(res)});
       }
     });
-   
-    
   }
-  componentWillUnmount(){
-    // ???什么时机触发？
-    let tasks = this.state.tasks;
+
+  _handleBackPress(navigator) {
+    navigator.pop();
   }
   upDateTime(index, value){
+    console.warn(index);
+    // console.warn(JSON.stringify(value));
     return new Promise((resolve, reject) => {
       let tasks = this.state.tasks;
-      tasks[index] = value;
+      tasks[index].time = value;
       AsyncStorage.setItem(storageId, JSON.stringify(tasks), () => {
         // console.warn(JSON.stringify(tasks));
         this.setState({tasks: tasks},()=>{
@@ -228,37 +280,89 @@ class Home extends Component{
     
     // alert(JSON.stringify(tasks));
   }
-  render() {
-    let tasks = this.state.tasks || taskList;
-    return(
-      <View style={styles.home}>
-        {
-          tasks.map((v ,k) => {
-            return (<Row navigator={this.props.navigator} 
-            id = {k}
-            key= {k} 
-            upDateTime = {this.upDateTime}
-            onPress={this._handleNextPress}
-            task = {v}
-            />);
-          })
+  
+  goHome(id,value,navigator){
+    // 利用await方式，可以保证state更新完毕再跳转。
+    let back = async ()=>{
+      await this.upDateTime(id, value).catch(
+        (err) => {
+          console.warn(err)
         }
-      </View>
-    );
+      );
+      this._handleBackPress(navigator);
+    };
+    back();
   }
-}
 
-export default class Life extends Component {
   render() {
+    const routes = [
+      {home:{},title:'首页',index:0}
+    ];
+
     return (
-      <NavigatorIOS
-        initialRoute={{
-          component: Home,
-          title: 'Life',
-          titleTextColor: '#fff',
-          barTintColor: '#3b5999'
+      <Navigator
+        ref={()=>{this.navigator = Navigator}}
+        initialRoute={routes[0]}
+        renderScene={(route, navigator) =>{
+          if(route.home){
+            return(
+              <Home navigator={navigator} upDateTime={this.upDateTime} tasks={this.state.tasks}/>
+            )
+          }
+          if(route.detail){
+            let id = route.detail.index;
+            return(
+              <Detail time={this.state.tasks[id].time} 
+              />
+            )
+          }
+         
         }}
-        style={styles.nav}
+        onDidFocus={(route,navigator)=>{
+          // console.warn(route.index);
+        }}
+
+
+
+        navigationBar={
+           <Navigator.NavigationBar
+             routeMapper={{
+               LeftButton: (route, navigator, index, navState) =>
+                {
+                  if (route.home) {
+                    return null;
+                  } else {
+                  var previousRoute = navState.routeStack[index - 1];
+                    return (
+                      <TouchableOpacity
+                        onPress={() => {
+                          this.goHome(route.detail.index,globT,navigator);
+                        }}
+                        style={styles.navBarLeftButton}>
+                        <Text style={[styles.navBarText, styles.navBarButtonText]}>
+                          {previousRoute.title}
+                        </Text>
+                      </TouchableOpacity>
+                    );
+                  }
+                },
+                RightButton: (route, navigator, index, navState) =>
+                { return (<Text></Text>); },
+                  Title: (route, navigator, index, navState) =>
+                {
+                if (route.home) { 
+                  return (<Text style={{color: '#fff'}}>Life</Text>); 
+                }else{
+                  return (<Text style={{color: '#fff'}}>修炼中</Text>) 
+                }
+              }
+             }}
+             style={{backgroundColor: '#3b5999'}}
+           />
+        }
+
+        configureScene={(route, routeStack) =>
+          Navigator.SceneConfigs.FloatFromBottom}
       />
     );
   }
@@ -333,7 +437,11 @@ const styles = StyleSheet.create({
     lineHeight: 40,
     color: '#19b955',
     fontSize: 30
-  }
+  },
+  navBarLeftButton: {
+    paddingLeft: 10,
+  },
+
 });
 
 AppRegistry.registerComponent('life', () => Life);
